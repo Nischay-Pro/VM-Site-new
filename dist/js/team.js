28f44f654dada7bc3198fb37f6b983634c83e178
Nischay Pro <NischayPro@yahoo.co.in>
2016-12-30 13:09:11 +0530

    Much needed Commit

---
+++ dist/js/team.js
@@ -0,0 +1,65 @@
+    function shreya() {
+      swal({
+        title: 'Shreya Nimma',
+        html: '<section class="body-text body-text--visible" data-component="body-text"><div class="body-text__inner"><h3 class="body-text__title">A Bit About Me</h3><div class="body-text__wysiwyg"><p>A current second year at BPHC, her love for language defines her.</p><p> After dabbling in the speaking arts when she was younger, she finds herself now most comfortable in front of a text editor,and more often than not, a fast approaching article deadline.</p><p> Those who work with her, complain that she likes Google Documents a little too much.</p><p class="body-text__quote">Insert Your Quote</p></div></div></section>',
+        imageUrl: './dist/img/team/1.jpg',
+        imageWidth: 200,
+        imageHeight: 200,
+        animation: true
+      })
+    }
+
+    function sumiran() {
+      swal({
+        title: 'Sumiran Mittal',
+        html: '<section class="body-text body-text--visible" data-component="body-text"><div class="body-text__inner"><h3 class="body-text__title">A Bit About Me</h3><div class="body-text__wysiwyg"><p>A voracious debater since high school, he discovered his aptitude for leadership right from his first year in university.</p><p>His passion for presentation and his love for debating, when combined can be a splendid combination.</p><p>And that’s why he\'s the Lord Excellency.</p><p class="body-text__quote">Insert Your Quote</p></div></div></section>',
+        imageUrl: './dist/img/team/2.jpg',
+        imageWidth: 200,
+        imageHeight: 200,
+        animation: true
+      })
+    }
+
+    function deek() {
+      swal({
+        title: 'Deekshith Pachipulusu',
+        html: '<section class="body-text body-text--visible" data-component="body-text"><div class="body-text__inner"><h3 class="body-text__title">A Bit About Me</h3><div class="body-text__wysiwyg"><p> He is a light-hearted guy with a zeal for entrepreneurship. Highly interested and actively engaged in Volleyball and Cricket.</p><p>He loves being a part of initiatives that focus on finance and management.</p><p>With a passion that burns like a comforting bonfire, you can always find him with a ready smile and cheery attitude.</p><p class="body-text__quote">Insert Your Quote</p></div></div></section>',
+        imageUrl: './dist/img/team/3.jpg',
+        imageWidth: 200,
+        imageHeight: 200,
+        animation: true
+      })
+    }
+
+    function momo() {
+      swal({
+        title: 'Monarch Moolchandani',
+        html: '<section class="body-text body-text--visible" data-component="body-text"><div class="body-text__inner"><h3 class="body-text__title">A Bit About Me</h3><div class="body-text__wysiwyg"><p>A current second year at BPHC, her love for language defines her.</p><p> After dabbling in the speakingarts when she was younger, she finds herself now most comfortable in front of a text editor,and more often than not, a fast approaching article deadline.</p><p> Those who work with her, complain that she likes Google Documents a little too much.</p><p class="body-text__quote">Insert Your Quote</p></div></div></section>',
+        imageUrl: './dist/img/team/4.jpg',
+        imageWidth: 200,
+        imageHeight: 200,
+        animation: true
+      })
+    }
+
+    function tushar() {
+      swal({
+        title: 'Tushar Kanth',
+        html: '<section class="body-text body-text--visible" data-component="body-text"><div class="body-text__inner"><h3 class="body-text__title">A Bit About Me</h3><div class="body-text__wysiwyg"><p>Interests lie in designing, coding and gaming. Loves Football more than anything. An Otaku.</p><p> Prefers freedom during work and likes to work as a team.</p><p>Approachable and chill with almost everything. Not good at managing work and time.</p><p class="body-text__quote">Insert Your Quote</p></div></div></section>',
+        imageUrl: './dist/img/team/5.jpg',
+        imageWidth: 200,
+        imageHeight: 200,
+        animation: true
+      })
+    }
+
+    function suchit() {
+      swal({
+        title: 'Suchit Kar',
+        html: '<section class="body-text body-text--visible" data-component="body-text"><div class="body-text__inner"><h3 class="body-text__title">A Bit About Me</h3><div class="body-text__wysiwyg"><p>A reluctant engineer, being fashioned from an avid reader and an occasional writer with a bug for quizzing.</p><p>Also, a painter with an eye for nature.</p><p>For all your event related queries, at your service.</p><p class="body-text__quote">Insert Your Quote</p></div></div></section>',
+        imageUrl: './dist/img/team/6.jpg',
+        imageWidth: 200,
+        imageHeight: 200,
+        animation: true
+      })
+    }
